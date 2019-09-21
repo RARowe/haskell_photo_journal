94777104e7a595eedb3899a5832ba0ec4333d99b
Richard Rowe <roweprogrammer@gmail.com>
2019-09-21 18:42:39 -0400

    Basic route helpers. Will need to clean up

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,9 @@ import qualified Network.Wai as W
 import Network.HTTP.Types
 import Network.Wai.Handler.Warp (run)
 import qualified Data.Aeson as A (encode)
+import qualified Data.Either as E (lefts)
+import qualified Data.List as L (intercalate)
+import qualified Data.Text as T (unpack)
 import qualified Migration as M (runMigrations)
 import qualified Models as MO (FilmRoll(..))
 import qualified Database.HDBC.Sqlite3 as S (connectSqlite3)
@@ -26,16 +29,16 @@ notFound = W.responseLBS
   headers
   "404: Not found."
 
-api :: W.Application
-api req res =  do
-  results <- withConnection getAllFilmRolls
+getAllFilmRolls :: W.Application
+getAllFilmRolls req res =  do
+  results <- withConnection getAllFilmRollsDb
   res $ W.responseLBS
     status200
     [("Content-Type", "application/json")]
     (A.encode results)
 
-getAllFilmRolls :: DB.IConnection conn => conn -> IO [MO.FilmRoll]
-getAllFilmRolls conn = do
+getAllFilmRollsDb :: DB.IConnection conn => conn -> IO [MO.FilmRoll]
+getAllFilmRollsDb conn = do
   rows <- DS.get conn "select Title, DateCreated from FilmRoll;" []
   return $ map DS.fromSqlRow rows
 
@@ -43,8 +46,47 @@ router :: W.Application
 router req res =
   case W.pathInfo req of
     [] -> res index
-    "api":path -> api req res
+    "api":path -> apiRouter req res
     _ -> res notFound
+
+type Route = (String, [Int])
+
+getUri :: Route -> String
+getUri (uri, _) = uri
+
+getIds :: Route -> [Int]
+getIds (_, ids) = ids
+
+makeRoute :: W.Request -> Route
+makeRoute req = (makeRouteUri segments, getRouteIds segments)
+  where segments = map (makeRouteSegment . T.unpack) $ W.pathInfo req
+
+makeRouteUri :: [Either Int String] -> String
+makeRouteUri segs = L.intercalate "/" $ map foo segs
+  where foo seg = case seg of
+                    (Left _) -> ":id"
+                    (Right val) -> val
+
+getRouteIds :: [Either Int String] -> [Int]
+getRouteIds segs = E.lefts segs
+
+makeRouteSegment :: String -> Either Int String
+makeRouteSegment seg =
+  case readInt seg of
+    (Just id) -> Left id
+    Nothing -> Right seg
+
+readInt :: String -> Maybe Int
+readInt s =
+  case reads s of
+    [(val, _)] -> Just val
+    _ -> Nothing
+
+apiRouter :: W.Application
+apiRouter req res =
+  case getUri $ makeRoute req of
+    "api/filmrolls" -> getAllFilmRolls req res
+    _ -> res index
 
 main :: IO ()
 main = do
